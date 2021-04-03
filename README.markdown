68ab9e8b5e747fb2c8b15815f0c943a6feb7b934
opaulofelipe <80161893+opaulofelipe@users.noreply.github.com>
2021-04-03 08:04:27 -0300

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Enciclopédia Geográfica
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/opaulofelipe/pen/ZELGdNb](https://codepen.io/opaulofelipe/pen/ZELGdNb).
+
+
---
+++ src/script.js
@@ -0,0 +1,59 @@
+var resposta = prompt("De qual Estado brasileiro você quer saber a capital?")
+
+if (resposta == "Acre") {
+  document.write("<h2>A capital do Acre é Rio Branco.</h2>")
+} else if (resposta == "Alagoas") {
+  document.write("<h2>A capital de Alagoas é Maceió.</h2>")
+} else if (resposta == "Amapá") {
+  document.write("<h2>A capital de Amapá é Macapá.</h2>")
+} else if (resposta == "Amazonas") {
+  document.write("<h2>A capital de Amazonas é Manaus.</h2>")
+} else if (resposta == "Bahia") {
+  document.write("<h2>A capital da Bahia é Salvador.</h2>")
+} else if (resposta == "Alagoas") {
+  document.write("<h2>A capital do Ceará é Fortaleza.</h2>")
+} else if (resposta == "Distrito Federal") {
+  document.write("<h2>A capital do Distrito Federal é Brasília.</h2>")
+} else if (resposta == "Espírito Santo") {
+  document.write("<h2>A capital do Espírito Santo é Vitória.</h2>")
+} else if (resposta == "Goiás") {
+  document.write("<h2>A capital de Goiás é Goiânia.</h2>")
+} else if (resposta == "Maranhão") {
+  document.write("<h2>A capital do Maranhão é São Luís.</h2>")
+} else if (resposta == "Mato Grosso") {
+  document.write("<h2>A capital de Mato Grosso é Cuiabá.</h2>")
+} else if (resposta == "Mato Grosso do Sul") {
+  document.write("<h2>A capital de Mato Grosso do Sul é Campo Grande.</h2>")
+} else if (resposta == "Minas Gerais") {
+  document.write("<h2>A capital de Minas Gerais é Belo Horizonte.</h2>")
+} else if (resposta == "Pará") {
+  document.write("<h2>A capital do Pará é Belém.</h2>")
+} else if (resposta == "Paraíba") {
+  document.write("<h2>A capital da Paraíba é João Pessoa.</h2>")
+} else if (resposta == "Paraná") {
+  document.write("<h2>A capital do Paraná é Curitiba.</h2>")
+} else if (resposta == "Pernambuco") {
+  document.write("<h2>A capital de Pernambuco é Recife.</h2>")
+} else if (resposta == "Piauí") {
+  document.write("<h2>A capital do Piauí é Teresina.</h2>")
+} else if (resposta == "Rio de Janeiro") {
+  document.write("<h2>A capital do Rio de Janeiro é Rio de Janeiro.</h2>")
+} else if (resposta == "Rio Grande do Norte") {
+  document.write("<h2>A capital do Rio Grande do Norte é Natal.</h2>")
+} else if (resposta == "Rio Grande do Sul") {
+  document.write("<h2>A capital do Rio Grande do Sul é Porto Alegre.</h2>")
+} else if (resposta == "Rondônia") {
+  document.write("<h2>A capital de Rondônia é Porto Velho.</h2>")
+} else if (resposta == "Roraima") {
+  document.write("<h2>A capital de Roraima é Boa Vista.</h2>")
+} else if (resposta == "Santa Catarina") {
+  document.write("<h2>A capital de Santa Catarina é Florianópolis.</h2>")
+} else if (resposta == "São Paulo") {
+  document.write("<h2>A capital de São Paulo é São Paulo.</h2>")
+} else if (resposta == "Sergipe") {
+  document.write("<h2>A capital de Sergipe é Aracaju.</h2>")
+} else if (resposta == "Tocantins") {
+  document.write("<h2>A capital de Tocantins é Palmas.</h2>")
+} else {
+  document.write("<h2>Esse estado não existe. Fique atento ao uso correto das letras maiúsculas e minúsculas.</h2>")
+}
